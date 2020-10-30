e973b5a161bfab4fa5fe9073371839d1fc9c965c
Pratyaksh Singh <43118103+Pratyaksh06@users.noreply.github.com>
2020-10-30 15:20:58 +0530

    Update subdir.py

--- WebSubDirBrute/subdir.py
+++ WebSubDirBrute/subdir.py
@@ -8,13 +8,14 @@ Created on Fri Oct 30 02:42:24 2020
 
 
 import requests
+import socket
 import sys
 user_input_url = sys.argv[1]
 wordlist = sys.argv[2]
 extension = sys.argv[3]
 
 def write(word):
-    f1=open("write1.txt","a")
+    f1=open("write1.txt")
     f1.write(word+"\n")
     
 fo = open(wordlist,"r+")
@@ -22,7 +23,7 @@ for i in range(2000):
     word = fo.readline(10).strip()
     surl = user_input_url+word+extension
     response = requests.get(surl)
-    if (response.status_code == 200):
+    if (response.status_code = 200):
         print ("[+] found :- ",surl)
         write(word)
     else:
