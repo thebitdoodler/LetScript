8fc2c302b2b08ec858ad1a0c979077ef0abecbd7
fr3qu3n533 <73750885+fr3qu3n533@users.noreply.github.com>
2020-10-31 21:31:07 +0530

    Update Web_Scraping.py

--- Web_Scraping/Web_Scraping.py
+++ Web_Scraping/Web_Scraping.py
@@ -1,6 +1,6 @@
 from selenium import webdriver # allow launching browser
 from selenium.webdriver.common.by import By # allow search with parameters
-from selenium.webdriver import WebDriverWait # allow waiting for page to load
+from selenium.webdriver.support.ui import WebDriverWait # allow waiting for page to load
 from selenium.webdriver.support import expected_conditions as EC # determine whether the web page has loaded
 from selenium.common.exceptions import TimeoutException # handling timeout situation 
 
@@ -18,13 +18,13 @@ browser.get("https://github.com/collections/machine-learning")
 
 
 # Extract all projects
-projects = browser.find_elements_by_xpath("//h1[@class='h3 lh-condensed']")
+projects = browser.find_elements_by_xpath("//h1[@class='h3 lh-condensed]")
 
 
 # Extract information for each project
 project_list = {}
 for proj in projects:
- proj_name = projtext # Project name
+ proj_name = proj.text # Project name
  proj_url = proj.find_elements_by_xpath("a")[0].get_attribute('href) # Project URL
  project_list[proj_name] = proj_url
  
@@ -34,7 +34,7 @@ browser.quit()
 
 
 # Extracting data
-project_df = pd.DataFramefrom_dict(project_list, orient = 'index')
+project_df = pd.DataFrame.from_dict(project_list, orient = 'index')
 
 
 # Manipulate the table
