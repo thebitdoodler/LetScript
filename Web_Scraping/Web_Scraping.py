044ff18dbccb7bc2ebb96ea18741d2f41a81a036
fr3qu3n533 <73750885+fr3qu3n533@users.noreply.github.com>
2020-10-31 21:40:31 +0530

    Update Web_Scraping.py

--- Web_Scraping/Web_Scraping.py
+++ Web_Scraping/Web_Scraping.py
@@ -9,24 +9,24 @@ driver_option = webdriver.ChromeOptions()
 driver_option.add_argument(" — incognito")
 chromedriver_path = '/Users/woratana/Downloads/chromedriver' # Change this to your own chromedriver path!
 def create_webdriver():
- return webdriver.Chrome(executable_path=chromedriver_path, chrome_options=driver_option)
+return webdriver.Chrome(executable_path=chromedriver_path, chrome_options=driver_option)
  
  
  # Open the website
 browser = create_webdriver()
-browser.get("https://github.com/collections/machine-learning")
+browser.get("https://github.com/collectionsmachine-learning")
 
 
 # Extract all projects
-projects = browser.findelements_by_xpath("//h1[@class='h3 lh-condensed']")
+projects = browser.find_elements_by_xpath("//h1[@class='h3 lh-condensed']")
 
 
 # Extract information for each project
 project_list = {}
 for proj in projects:
- proj_name = proj.text # Project name
- proj_url = proj.find_elements_by_xpath("a")[1].get_attribute('href') # Project URL
- project_list[proj_name] = proj_url
+ proj_name = projj.text # Project name
+ proj_url = proj.find_elements_by_xpath("a")[0].get_attribute('href') # Project URL
+ project_list[projname] = proj_url
  
  
  # Close connection
@@ -43,5 +43,5 @@ project_df.column = ['project_url', 'project_name']
 project_df = project_df.reset_index(drop=True)
 
 # Export project dataframe to CSV
-project_df.to_csv('project_list.csv')
+project_f.to_csv('project_list.csv')
 	
