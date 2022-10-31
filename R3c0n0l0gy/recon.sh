413fdbae4454756988c7ac3a7fd6c853cb2e9e39
Pratyaksh Singh <pratyaksh.11702109@lpu.in>
2022-10-31 21:20:14 +0530

    Update recon.sh
    Made changes
    1. Adding directory
    2. Finding alive domains

--- R3c0n0l0gy/recon.sh
+++ R3c0n0l0gy/recon.sh
@@ -1,16 +1,19 @@
 #/bin/bash
 
 
-_target=$1
+#_target=$1
 
-echo "$_target"
+#echo "$_target"
 
-assetfinder --subs-only $_target | anew subdomains.txt
+# Finding alive sub-domains
+ 
+mkdir -p _target=$1
+
+cd $_target
+
+assetfinder --subs-only $_target | httpx | anew $_target/subdomains.txt
 
 #curl "https://crt.sh/?q=$_target&output=json" | jq ".[].name_value" | sed 's/\"//g' | sed 's/\*\.//g' | sort -u > subdomains.txt
 
 #performing port scanning via naabu
 naabu -list subdomains.txt -o /tmp/naabu.txt
-
-
-
